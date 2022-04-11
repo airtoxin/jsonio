30afc978c37235f132ee1ae11072512b881f5d89
airtoxin <airtoxin@icloud.com>
2022-04-11 21:43:26 +0900

    Rename

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  pageExtensions: ["page.jsx", "page.js", "page.tsx", "page.ts"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
